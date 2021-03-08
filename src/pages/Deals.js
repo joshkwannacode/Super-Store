9ffbe0a43a2fee3faa9f83d0758ebd959f3b0a9f
Josh <forkjerkjork@gmail.com>
2021-03-08 18:04:31 +0100

    fixed bugs in Deals

--- src/pages/Deals.js
+++ src/pages/Deals.js
@@ -1,22 +1,19 @@
-import React,{useContext,useState,useEffect} from 'react';
-import {Context} from '../context/Context';
+import React,{useState,useEffect} from 'react';
 import ItemCard from '../components/itemCard';
 import getItemList from '../api/getItemList';
 
 export default function Deals() {
-    const {items,setItems} = useContext(Context);
     const [filteredItems,setFilteredItems] = useState([]);
     
-    // should I call the api here too instead of using context?
     useEffect(() => {
         getItemList()
-        .then(res=>console.log(res))
+        .then(res=>setFilteredItems(res.filter(saleItems=>saleItems.isOnSale===true)))
     },[])
 
     console.log("deals",filteredItems);
     return (
-        <div>
+        <>
             <ItemCard details={filteredItems}/>
-        </div>
+        </>
     )
 }
