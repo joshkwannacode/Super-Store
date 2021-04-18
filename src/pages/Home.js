9aa0670b197c4198a830c85957143aa83d7feef6
Josh <forkjerkjork@gmail.com>
2021-04-18 01:32:49 -0700

    finished search function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,46 @@
-import React, {useState,useEffect,useContext} from 'react';
+import React, {useState,useEffect} from 'react';
 import ItemCard from '../components/itemCard/index';
 import getItemList from '../api/getItemList';
-import {Context} from '../context/Context';
+import SearchBar from '../components/SearchBar';
 
 export default function Home() {
     const [items,setItems] = useState([]);
-    
+    const [searchItem, setSearchItem] = useState();
+    const [itemBackUp, setItemBackUp] = useState([]);
+
     useEffect(()=>{
         getItemList()
-        .then(res=>setItems(res.items))
+        .then(res=>{
+            setItems(res.items)
+            setItemBackUp(res.items)})
     },[])
 
-    console.log(items)
+    const handleSearch =(e)=>{
+        setSearchItem(e.target.value);
+        const filtereditems = e.target.value?items.filter(item =>{
+            return item.name.toLowerCase().includes(e.target.value.toLowerCase());
+          }):itemBackUp;
+        const check = filtereditems.length>0;
 
+        if(check){
+              setItems(filtereditems);
+        } 
+          
+    };
+    const handleSubmit=()=>{
+        const filtereditems = searchItem?items.filter(item =>{
+                    return item.name.toLowerCase().includes(searchItem.toLowerCase());
+                  }):itemBackUp;
+        
+        const check = filtereditems.length>0;
+
+        if(check){
+              setItems(filtereditems);
+        } 
+    };
     return (
         <div>
+            <SearchBar handleSearch={handleSearch} handleSubmit={handleSubmit} />
             {items.map((item)=>{
                 return(
                 <ItemCard details={item}/>
